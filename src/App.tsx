42e897c9a81555d8c0ec6a297d89b2939ab0d16f
Michel Gouveia <michel-gouveia1997@hotmail.com>
2026-03-28 08:34:58 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import { useState } from 'react'
-
+import {populateCurrencySelect} from '../src/Currencies/Currencies'
+import { convertCurrency } from './CurrencyService/CurrencyService'
 
 function App() {
 
-
   return (
       <>
-      <h1>hi there</h1>
+      <div className="container_currency">
+        <h1>Currency Converter</h1>
+         <div className="container">
+              <div className="currency_box">
+                   <label htmlFor="amount">Amount</label>
+                   <input id="amount" type="text" placeholder="0.00"></input>
+              </div>
+              <div className="currency_box">
+                  <label htmlFor="from">From</label>
+                  <select id="from"></select> 
+              </div>
+          <button id="swap" type="button">
+             <i className="fa-solid fa-right-left"></i>
+          </button>
+       <div className="currency_box">
+         <label htmlFor="to">To</label>
+         <select id="to"></select> 
+      </div>
+    </div>
+      </div>
       </>
   )
 }
---
+++ src/CurrencyConverter/CurrencyConverter.ts
@@ -0,0 +1,37 @@
+import { convertCurrency } from '../CurrencyService/CurrencyService'
+import type { currencyConversionRequest,currencyConversionResult } from '../Types/Types'
+
+export class CurrencyConverter {
+ 
+    private history:currencyConversionResult[] = []
+
+    constructor(){
+      const data =   localStorage.getItem("history")
+
+      this.history = data ? JSON.parse(data): [];
+    }
+    async convert(
+    request: currencyConversionRequest
+   ): Promise<currencyConversionResult> {
+
+    const result = await convertCurrency(request)
+
+    this.history.push(result)
+
+    this.saveHistory()
+
+    console.log(result)
+
+    return result
+  }
+  private saveHistory(){
+        localStorage.setItem("history",JSON.stringify(this.history))
+  }
+  getHistory():currencyConversionResult[]{
+        return  this.history
+  }
+}
+
+
+
+
