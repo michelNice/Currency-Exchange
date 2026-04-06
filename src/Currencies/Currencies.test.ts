28c6ddd0b52b20b888e49b448f5fe978300162df
Michel Gouveia <michel-gouveia1997@hotmail.com>
2026-04-06 16:54:07 -0300

    update

---
+++ src/Currencies/Currencies.test.ts
@@ -0,0 +1,19 @@
+import { populateCurrencySelect } from "./Currencies"
+
+test("should return formatted currencies", () => {
+
+  const result = populateCurrencySelect()
+
+  expect(result).toContainEqual({
+    code: "USD",
+    symbol: "$",
+    flag: "us"
+  })
+
+  expect(result).toContainEqual({
+    code: "BRL",
+    symbol: "R$",
+    flag: "br"
+  })
+
+})
