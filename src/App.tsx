dbdd337cfe9405c74ac24114662dfed70e82d697
Michel Gouveia <michel-gouveia1997@hotmail.com>
2026-04-10 07:19:52 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -178,23 +178,30 @@ function App() {
 
   </div>
       </div>
+    
+    {history.length > 0 && (
+  <>
     <h2 className='history_h2'>Conversion history</h2>
 
-<div className="history">
-  {history.slice(-3).reverse().map((item, index) => (
-    <div key={index} className="history_item">
-      <div className="history_from">
-        {formatCurrency(item.originalAmount)} {item.fromCurrency}
-      </div>
+    <div className="history">
+      {history.slice(-3).reverse().map((item, index) => (
+        <div key={index} className="history_item">
+          <div className="history_from">
+            {formatCurrency(item.originalAmount)} {item.fromCurrency}
+          </div>
 
-      <div className="history_arrow">→</div>
+          <div className="history_arrow">→</div>
 
-      <div className="history_to">
-        {formatCurrency(item.convertedAmount)} {item.toCurrency}
-      </div>
+          <div className="history_to">
+            {formatCurrency(item.convertedAmount)} {item.toCurrency}
+          </div>
+        </div>
+      ))}
     </div>
-  ))}
-</div>
+  </>
+)}
+
+
   </>
   )
 }
